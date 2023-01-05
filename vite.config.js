e094d7794c1c869ade26c2d11be536e513a732f2
ArepaZombie <jorgetorresmora99@gmail.com>
2023-01-05 01:20:10 -0500

    triyng to fix this

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   publicPath:process.env.NODE_ENV === 'production'
-  ? 'album-display/'
-  : '/'
+  ? 'album-display/.'
+  : '/.'
 })
