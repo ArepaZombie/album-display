98799c000c9bda22227d7cbeae59f4f77278c5bd
ArepaZombie <jorgetorresmora99@gmail.com>
2023-01-05 00:52:34 -0500

    a

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicPath: "/album-display"
+  publicPath: "/album-display/"
 })
